0005d7a15e61768eeaef9d3888723d1b7a652cb5
Ryan Connor <40437041@live.napier.ac.uk>
2023-01-11 15:12:09 +0000

    Adding Testing Project

---
+++ HonoursTests/DistanceFormulaTests.cs
@@ -0,0 +1,18 @@
+using HonoursProjectAlgorithmComparer;
+
+namespace HonoursTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
